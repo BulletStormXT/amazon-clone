24e4ee9279791c354a23c48c7872fc86d29a9fb9
BulletStormXT <BulletStormXT@gmail.com>
2024-02-27 15:29:20 +0100

    0.50 2024-02-27 - 15-30
    Detailinformationen für Artikel

--- amazon-clone-vite/src/pages/Home.jsx
+++ amazon-clone-vite/src/pages/Home.jsx
@@ -185,74 +185,6 @@ Home.propTypes = {
 
 export default Home; */
 
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import ShoppingCart from "./ShoppingCart";
-import Header from "../components/Header/Header";
-// import { Link } from "react-router-dom";
-
-const Home = ({ products, addToCart, itemCount }) => {
-  if (!Array.isArray(products) || products.length === 0) {
-    return <div>No products available</div>;
-  }
-
-  const categories = [...new Set(products.map((product) => product.category))];
-
-  return (
-    <div>
-      <h1>Categories</h1>
-      <div className="category-container">
-        {categories.map((category) => (
-          <div key={category} className="category">
-            <a href={`/products?category=${category}`}>{category}</a>
-          </div>
-        ))}
-      </div>
-      <h1>Products</h1>
-      <div className="product-container">
-        {products.map((product) => (
-          <div key={product.id} className="product">
-            <img src={product.image} alt={product.name} width={"250px"} />
-            <div className="product-details">
-              <h3>{product.name}</h3>
-              <p>{product.category}</p>
-              <p>Price: {product.price}€</p>
-              <div>
-                <button
-                  className="cart-button"
-                  onClick={() => {
-                    addToCart();
-                  }}
-                >
-                  Add to Cart
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h2>Cart</h2>
-        <p>Item count: {itemCount}</p>
-      </div>
-    </div>
-  );
-};
-
-Home.propTypes = {
-  products: PropTypes.arrayOf(
-    PropTypes.shape({
-      category: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
-      image: PropTypes.string.isRequired,
-    })
-  ),
-};
-
-export default Home;
-
 /* const Home = ({ products }) => {
   const [cartItems, setCartItems] = useState([]);
 
@@ -375,3 +307,169 @@ Home.propTypes = {
 };
 
 export default Home; */
+
+// import { Link } from "react-router-dom";
+/* import React, { useState } from "react";
+import PropTypes from "prop-types";
+import ShoppingCart from "./ShoppingCart";
+import Header from "../components/Header/Header";
+
+const Home = ({ products, addToCart, itemCount }) => {
+  if (!Array.isArray(products) || products.length === 0) {
+    return <div>No products available</div>;
+  }
+
+  const categories = [...new Set(products.map((product) => product.category))];
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <div className="category-container">
+        {categories.map((category) => (
+          <div key={category} className="category">
+            <a href={`/products?category=${category}`}>{category}</a>
+          </div>
+        ))}
+      </div>
+      <h1>Products</h1>
+      <div className="product-container">
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <img src={product.image} alt={product.name} width={"250px"} />
+            <div className="product-details">
+              <h3>{product.name}</h3>
+              <p>{product.category}</p>
+              <p>Price: {product.price}€</p>
+              <div>
+                <button
+                  className="cart-button"
+                  onClick={() => {
+                    addToCart();
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Cart</h2>
+        <p>Item count: {itemCount}</p>
+      </div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Home; */
+
+// WIP: add PUC
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import ShoppingCart from "./ShoppingCart";
+import Header from "../components/Header/Header";
+import db from "../db.json";
+
+const Home = ({ products, addToCart, itemCount }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+    toggleCart();
+  };
+
+  if (!Array.isArray(products) || products.length === 0) {
+    return <div>No products available</div>;
+  }
+
+  const categories = [...new Set(products.map((product) => product.category))];
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <div className="category-container">
+        {categories.map((category) => (
+          <div key={category} className="category">
+            <a href={`/products?category=${category}`}>{category}</a>
+          </div>
+        ))}
+      </div>
+      <h1>Products</h1>
+      <div className="product-container">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="product"
+            onClick={() => handleProductClick(product)}
+          >
+            <img src={product.image} alt={product.name} width={"250px"} />
+            <div className="product-details">
+              <h3>{product.name}</h3>
+              <p>{product.category}</p>
+              <p>Price: {product.price}€</p>
+              <div>
+                <button
+                  className="cart-button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    addToCart();
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {isCartOpen && selectedProduct && (
+        <div className="popup-cart">
+          <h2>{selectedProduct.name}</h2>
+          <div className="product-details">
+            <h3>Details</h3>
+            <p>{selectedProduct.details}</p>
+          </div>
+          <button onClick={toggleCart}>Close</button>
+        </div>
+      )}
+      <div>
+        <h2>Cart</h2>
+        <p>Item count: {itemCount}</p>
+      </div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Home;
+
+// Sony Walkman: f/b v
