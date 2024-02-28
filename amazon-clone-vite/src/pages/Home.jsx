ebc18e1d0c15dbbb0f13d2038d0ccb214c9c5aba
BulletStormXT <BulletStormXT@gmail.com>
2024-02-28 10:39:06 +0100

    0.65 - 2024-02-28 - 10-45

--- amazon-clone-vite/src/pages/Home.jsx
+++ amazon-clone-vite/src/pages/Home.jsx
@@ -376,8 +376,8 @@ Home.propTypes = {
 
 export default Home; */
 
-// WIP: add PUC
-import React, { useState } from "react";
+// ############################################## WIP: add PUC
+/* import React, { useState } from "react";
 import PropTypes from "prop-types";
 import ShoppingCart from "./ShoppingCart";
 import Header from "../components/Header/Header";
@@ -470,6 +470,108 @@ Home.propTypes = {
   ),
 };
 
+export default Home; */
+
+// ############################################## w/ PUC
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import ShoppingCart from "./ShoppingCart";
+import Header from "../components/Header/Header";
+import db from "../db.json";
+
+const Home = ({ products, addToCart, itemCount }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cartPosition, setCartPosition] = useState({ x: 0, y: 0 });
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const handleProductClick = (product, event) => {
+    setSelectedProduct(product);
+    const rect = event.target.getBoundingClientRect();
+    setCartPosition({ x: rect.left, y: rect.top });
+    toggleCart();
+  };
+
+  if (!Array.isArray(products) || products.length === 0) {
+    return <div>No products available</div>;
+  }
+
+  const categories = [...new Set(products.map((product) => product.category))];
+
+  return (
+    <div style={{ margin: "40px" }}>
+      {/* <h3>Categories</h3> */}
+      <div className="category-container">
+        {categories.map((category) => (
+          <div key={category} className="category">
+            <a href={`/products?category=${category}`}>{category}</a>
+          </div>
+        ))}
+      </div>
+      {/* <h2>Products</h2> */}
+      <div className="product-container">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="product"
+            onClick={(e) => handleProductClick(product, e)}
+          >
+            <img src={product.image} alt={product.name} width={"250px"} />
+            <div className="product-details">
+              <h3>{product.name}</h3>
+              <p>{product.category}</p>
+              <p>Price: {product.price}€</p>
+              <div>
+                <button
+                  className="cart-button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    addToCart();
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {isCartOpen && selectedProduct && (
+        <div
+          className="popup-cart"
+          style={{ top: cartPosition.y, left: cartPosition.x }}
+        >
+          <h2>{selectedProduct.name}</h2>
+          <div className="product-details">
+            <h3>Details</h3>
+            <p>{selectedProduct.details}</p>
+          </div>
+          <button onClick={toggleCart}>Close</button>
+        </div>
+      )}
+      <div>
+        <h2>Cart</h2>
+        <p>Item count: {itemCount}</p>
+      </div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ),
+};
+
 export default Home;
 
-// Sony Walkman: f/b v
+// AFK
